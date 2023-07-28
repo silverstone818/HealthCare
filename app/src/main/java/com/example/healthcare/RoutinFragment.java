5d14fc3b62898f188bdb63f27f13696ea822b081
HanRoseA <112860641+Hanrosea@users.noreply.github.com>
2023-07-28 19:16:41 +0900

    routin_YV

--- app/src/main/java/com/example/healthcare/RoutinFragment.java
+++ app/src/main/java/com/example/healthcare/RoutinFragment.java
@@ -1,25 +1,33 @@
 package com.example.healthcare;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class RoutinFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private WebView webView_1, webView_2, webView_3;
+    private View mCustomView;
+    private WebChromeClient.CustomViewCallback mCustomViewCallback;
+
     private String mParam1;
     private String mParam2;
-    private WebView webView;
 
     public RoutinFragment() {
     }
@@ -46,21 +54,88 @@ public class RoutinFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_routin, container, false);
 
-        // WebView 초기화를 합니다.
-        webView = view.findViewById(R.id.webView); // 여기서 수정: view.findViewById를 사용하여 view 내부에서 webView를 찾습니다.
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // JavaScript 사용 설정
-        webView.setWebChromeClient(new WebChromeClient()); // 웹뷰를 사용하여 동영상을 재생하기 위해 WebChromeClient 설정
+        webView_1 = view.findViewById(R.id.webView_1);
+        webView_2 = view.findViewById(R.id.webView_2);
+        webView_3 = view.findViewById(R.id.webView_3);
 
-        // YouTube 동영상 ID를 가져와서 아래와 같이 설정합니다.
-        String videoId = "YOUR_YOUTUBE_VIDEO_ID"; // 여기서 수정: 원하는 YouTube 동영상의 ID로 변경합니다.
-        String embedUrl = "https://www.youtube.com/watch?v=" + videoId;
-
-        String html = "<iframe width=\"100%\" height=\"100%\" src=\"" + embedUrl + "\" frameborder=\"0\" allowfullscreen></iframe>";
-        webView.loadData(html, "text/html", "utf-8");
+        initWebView(webView_1, "https://www.youtube.com/embed/2rb3GZw0IEw");
+        initWebView(webView_2, "https://www.youtube.com/embed/7Melm8LEFqY");
+        initWebView(webView_3, "https://www.youtube.com/embed/KCAwey51gUc");
 
         return view;
     }
+
+    private void initWebView(WebView webView, String url) {
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onHideCustomView() {
+                ((FrameLayout) getActivity().getWindow().getDecorView()).removeView(mCustomView);
+                mCustomView = null;
+                mCustomViewCallback.onCustomViewHidden();
+                mCustomViewCallback = null;
+
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                if (mCustomView != null) {
+                    callback.onCustomViewHidden();
+                    return;
+                }
+
+                mCustomView = view;
+                mCustomView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.MATCH_PARENT));
+                ((FrameLayout) getActivity().getWindow().getDecorView()).addView(mCustomView);
+                mCustomViewCallback = callback;
+
+                getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.loadData("<html><body><iframe src=\"" + url + "\" width=\"100%\" height=\"100%\"></iframe></body></html>", "text/html", "UTF-8");
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
+                    if (mCustomView != null) {
+                        WebChromeClient.CustomViewCallback callback = mCustomViewCallback;
+                        webView_1.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+                        webView_2.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+                        webView_3.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+                        callback.onCustomViewHidden();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
 }
